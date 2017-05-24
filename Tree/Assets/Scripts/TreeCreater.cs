21b78cd19fcd0651256c369ef49f3d80c586886b
Gavin Cai <caigavin@gmail.com>
2017-05-24 16:33:09 -0700

    Added terrain and ability to generate trees on click.

---
+++ Tree/Assets/Scripts/TreeCreater.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeCreater : MonoBehaviour {
+
+    public GameObject tree; 
+	
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit; 
+            if (Physics.Raycast(ray, out hit))
+            {
+                Instantiate(tree, hit.point, transform.rotation); 
+            }
+        }
+	}
+}

c8f499221011eb9935158472ef5c75ee96fdcee9
Gavin Cai <caigavin@gmail.com>
2017-06-27 23:39:23 -0700

    Added ability to create two different types of trees on click

--- Tree/Assets/Scripts/TreeCreater.cs
+++ Tree/Assets/Scripts/TreeCreater.cs
@@ -1,19 +1,32 @@
-﻿using System.Collections;
+﻿/*
+ * Prints out a different tree based on left or right mouse click. 
+ * 
+ * Author: Gavin Cai
+ * Date Last Edited: 27 June 2017
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TreeCreater : MonoBehaviour {
 
-    public GameObject tree; 
+    public GameObject sycamoreTree;
+    public GameObject tree2; 
 	
 	void Update () {
-		if (Input.GetMouseButtonDown(0))
-        {
+		if (Input.GetMouseButtonDown(0)) {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit; 
-            if (Physics.Raycast(ray, out hit))
-            {
-                Instantiate(tree, hit.point, transform.rotation); 
+            if (Physics.Raycast(ray, out hit)) {
+                Instantiate(sycamoreTree, hit.point, transform.rotation); 
+            }
+        }
+        if (Input.GetMouseButtonDown(1)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+                Instantiate(tree2, hit.point, transform.rotation);
             }
         }
 	}
